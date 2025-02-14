bf40e36b10e02dda8f34b4f59b18f9675db4d3d6
aminelli <antonio.minelli@etlforma.com>
2025-02-14 12:49:51 +0100

    upd

--- GR01/firebase_01/lib/main.dart
+++ GR01/firebase_01/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
@@ -21,11 +22,22 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase: San Valentino',
+      theme: ThemeData(
+        buttonTheme: Theme.of(context).buttonTheme.copyWith(highlightColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      /*
+      theme: ThemeData.dark(),
+      */
+      /*
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      */
       home: null,
     );
   }
