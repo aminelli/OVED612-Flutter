422587b43f953f8327a5f8350c5ee6b679c28ac3
aminelli <antonio.minelli@etlforma.com>
2025-02-25 16:39:01 +0100

    upd

--- GR02/firebase_01/lib/routing/routing.dart
+++ GR02/firebase_01/lib/routing/routing.dart
@@ -1,12 +1,26 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:go_router/go_router.dart';
 import '../screens/home_page_screen.dart';
 
-
+GoRoute signInRoute = GoRoute(
+  name: 'signIn',
+  path: 'sign-in',
+  builder: (context, state) {
+    return SignInScreen(
+      actions: [
+        ForgotPasswordAction(callback)
+      ]
+    );
+  },
+);
 
 GoRoute homeRoute = GoRoute(
-  name: 'home',
-  path: '/',
-  builder: (context, state) => const HomePage(),
+    name: 'home',
+    path: '/',
+    builder: (context, state) => const HomePage(),
+    routes: [
+
+  ]
 );
 
 final appRouter = GoRouter(
